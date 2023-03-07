45e66781d5a34c31bd7ffd1e4e2223401ec922d1
Ben Colquhoun <benjamincahoonz@gmail.com>
2023-03-07 13:50:13 +1300

    feat: Duplicated value report

--- EDA.py
+++ EDA.py
@@ -1,23 +1,29 @@
-# Data handling
-import numpy as np
 import pandas as pd
-
-# Data Investigation
-# import sklearn
-
-# Data visualisation
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def report_basic_info(df: pd.DataFrame):
+    """ Prints the information of the columns in the dataframe, then prints the statistical variation of the
+    columns. """
     print(df.info())
     print(df.describe())
 
 
+def report_duplicated(df: pd.DataFrame):
+    """ Prints the number duplicated instances over all columns, then excludes id, then excludes strength. """
+    df_size = len(df)
+    total_duped = sum(df.duplicated())
+    id_duped = sum(df.duplicated(subset=df.columns.to_list()[1:]))
+    id_str_duped = sum(df.duplicated(subset=df.columns.to_list()[1:-1]))
+    print(f"Full dupes: {total_duped} ({total_duped / df_size * 100:.1f}%)\n"
+          f"Without ID dupes: {id_duped} ({id_duped / df_size * 100:.1f}%)\n"
+          f"Feature dupes: {id_str_duped} ({id_str_duped / df_size * 100:.1f}%)")
+
+
 def main():
+    """ Main function to run the various EDA tasks. """
     df = pd.read_csv("Kaggle Data/train.csv")
     report_basic_info(df)
+    report_duplicated(df)
 
 
 if __name__ == "__main__":
