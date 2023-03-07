fb3f98ec2c923513cda0067af3e2ce503eca243b
Ben Colquhoun <benjamincahoonz@gmail.com>
2023-03-07 14:20:45 +1300

    feat: Duplicate investigation

--- EDA.py
+++ EDA.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def report_basic_info(df: pd.DataFrame):
@@ -19,12 +21,24 @@ def report_duplicated(df: pd.DataFrame):
           f"Feature dupes: {id_str_duped} ({id_str_duped / df_size * 100:.1f}%)")
 
 
+def plot_duplicated_distributions(df: pd.DataFrame):
+    """ Plots the distributions of the duplicated values. """
+    df_duplicated_count = df.groupby(df.columns.tolist()[1:-1], as_index=False).size()
+    df_duplicated_count = df_duplicated_count[df_duplicated_count['size'] != 1]
+    v_counts = df_duplicated_count['size'].value_counts()
+    sns.barplot(x=v_counts.keys().to_list(), y=v_counts.values)
+    plt.xlabel("Times repeated")
+    plt.ylabel("Row Count")
+    plt.title("Distribution of times a row is repeated")
+    plt.show()
+
+
 def main():
     """ Main function to run the various EDA tasks. """
     df = pd.read_csv("Kaggle Data/train.csv")
     report_basic_info(df)
     report_duplicated(df)
-
+    plot_duplicated_distributions(df)
 
 if __name__ == "__main__":
     main()
