1175b1435293eb02a1de1b77c4994f227f240325
Ben Colquhoun <benjamincahoonz@gmail.com>
2023-03-09 11:48:23 +1300

    feat: Duplicate instance handler
    Currently unused

--- main.py
+++ main.py
@@ -15,10 +15,28 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import Ridge
 
 
+def remove_duplicate_instances(df: pd.DataFrame):
+    """ The transform finds each set of duplicates and replaces them with a new entry with the mean objective value. """
+    feature_list = df.columns.tolist()[1:-1]
+    # Initialising clean data and dropping id
+    cleaned_df = df.drop_duplicates(subset=feature_list, keep=False).reset_index()
+    cleaned_df.drop(labels=["id", "index"], axis='columns', inplace=True)
+    # Duplicate handling
+    for idx, group in df[df.duplicated(feature_list, keep=False)].groupby(feature_list):
+        average_instance = group.iloc[1, 1:]
+        average_instance['Strength'] = group['Strength'].mean()
+        pd.concat([cleaned_df, average_instance], axis='index')
+    return cleaned_df
+
+
 def main():
     # Enable GPU processing
     patch_sklearn()
 
+    # Load and clean data
+    full_data = pd.read_csv("Kaggle Data/train.csv")
+    # full_data = remove_duplicate_instances(full_data)
+    full_data.drop(columns=["id"], inplace=True)
 
 if __name__ == "__main__":
     main()
