64d7e476dafbb178331879fcb9a8c11962069755
Ben Colquhoun <benjamincahoonz@gmail.com>
2023-03-09 13:25:19 +1300

    feat: Model Score function
    Takes a model and runs KFold validation on it to test the performance.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import pandas as pd
 import seaborn as sns
 import sklearn.metrics
 from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 # ML packages
 from sklearnex import patch_sklearn
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import Ridge
@@ -73,6 +74,31 @@ def create_training_pipeline():
                           ("preprocessing", create_preprocessing_pipeline()),
                           ("model", Ridge(70))])
     return clf
+
+
+def score_model_using_KFold(model, train, features, target, verbose: bool = True):
+    """ Runs KFold validation on the given model.
+    This code is inspired/used from https://www.kaggle.com/code/ambrosm/pss3e9-eda-which-makes-sense
+    :param train: The training dataset
+    :param test: The testing dataset
+    """
+    fold_scores = []
+    kf = KFold()
+    for fold_counter, (idx_train, idx_validation) in enumerate(kf.split(train)):
+        X_train = train.iloc[idx_train, features]
+        y_train = train.iloc[idx_train, target]
+        X_validation = train.iloc[idx_validation, features]
+        y_validation = train.iloc[idx_validation, target]
+
+        model.fit(X_train, y_train)
+        validation_RMSE = mean_squared_error(y_validation, model.predict(X_validation), squared=False)
+        fold_scores.append(validation_RMSE)
+
+        # Reporting
+        if verbose:
+            training_RMSE = mean_squared_error(y_train, model.predict(X_train), squared=False)
+            print(f"Fold: {fold_counter}\tTraining RMSE: {training_RMSE:.2f}\tValidation RMSE: {validation_RMSE:.2f}")
+    print(f"Average RMSE: {np.mean(fold_scores)}")
 
 
 def main():
